3befe5101617012c49f863e60e85fd02190d82ab
aschultzen <aschultzen@gmail.com>
2018-11-26 12:20:07 +0000

    Renamed and cleaned up

--- clock.py
+++ clock.py
@@ -1,76 +1,90 @@
-
-#!/usr/bin/env python
-
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
+
+#  A
+# F B
+#  G
+# E C
+#  D DP
+#
+# The lines over are supposed to be
+# a "drawing" of the segments and
+# their respective positions.
+
+# Segments HEX codes
+SEG_DP = 0x1
+SEG_B = 0x2
+SEG_C = 0x4
+SEG_D = 0x8
+SEG_E = 0x10
+SEG_F = 0x40
+SEG_G = 0x20
+SEG_A = 0x80
+
+# Number HEX codes
+ZERO = SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F
+ONE = SEG_B + SEG_C
+TWO = SEG_A + SEG_B + SEG_G + SEG_E + SEG_D
+THREE = SEG_A + SEG_B + SEG_G + SEG_C + SEG_D
+FOUR = SEG_F + SEG_B + SEG_G + SEG_C
+FIVE = SEG_A + SEG_F + SEG_G + SEG_C + SEG_D
+SIX = SEG_A + SEG_G + SEG_C + SEG_D + SEG_E + SEG_F
+SEVEN = ONE + SEG_A
+EIGHT = ZERO + SEG_G
+NINE = EIGHT - SEG_E  - SEG_D
+
+# Letters
+A = EIGHT - SEG_D
+B = EIGHT - SEG_B - SEG_A
+C = ZERO - SEG_B - SEG_C - SEG_G
+D = EIGHT - SEG_F - SEG_A
+E = EIGHT - SEG_B + SEG_C
+F = EIGHT - SEG_D - SEG_C - SEG_B
+
+numbers = [ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE]
+letters = [A, B, C, D, E, F]
+clearpanel = [0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+zeropanel = [ZERO, ZERO, ZERO, ZERO, ZERO, ZERO]
 
 GPIO.setmode(GPIO.BOARD)
-segmentClock=11
-segmentLatch=13
-segmentData=15
+PIN_DATA  = 15
+PIN_LATCH = 13
+PIN_CLOCK = 11
 
-GPIO.setup(segmentClock,GPIO.OUT)
-GPIO.setup(segmentData,GPIO.OUT)
-GPIO.setup(segmentLatch,GPIO.OUT)
+GPIO.setup(PIN_DATA,  GPIO.OUT)
+GPIO.setup(PIN_LATCH, GPIO.OUT)
+GPIO.setup(PIN_CLOCK, GPIO.OUT)
 
-GPIO.output(segmentClock,GPIO.LOW)
-GPIO.output(segmentData,GPIO.LOW)
-GPIO.output(segmentLatch,GPIO.LOW)
+def shiftout(byte):
+  GPIO.output(PIN_LATCH, 0)
+  for x in range(8):
+    GPIO.output(PIN_DATA, (byte >> x) & 1)
+    GPIO.output(PIN_CLOCK, 1)
+    GPIO.output(PIN_CLOCK, 0)
+  GPIO.output(PIN_LATCH, 1)
 
-number=0
+def drawinteger(integer):
+  panel = clearpanel
+  intlist = list(str(integer))
+  intlist = intlist[::-1]
+  for x in range(0,len(intlist)):
+    panel[x] = numbers[int(intlist[x])]
 
-def showNumber(value):
-    number = abs(value) #Remove negative signs and any decimals
-    x=0
-    while(x<2):
-        remainder=number % 10
-        postNumber(remainder, False)
-        number = number/10
-        x += 1
+  print(panel)
 
-    #Latch the current segment data
-    GPIO.output(segmentLatch,GPIO.LOW)
-    GPIO.output(segmentLatch,GPIO.HIGH) #Register moves storage register on the rising edge of RCK
+  for x in range(0, 6):
+    shiftout(0)
 
-#Given a number, or - shifts it out to the display
-def postNumber(number, decimal):
-    segments=bytes()
-    a=1<<0
-    b=1<<6
-    c=1<<5
-    d=1<<4
-    e=1<<3
-    f=1<<1
-    g=1<<2
-    dp=1<<7
-    if   number == 1: segments =     b | c                  
-    elif number == 2: segments = a | b |     d | e |     g 
-    elif number == 3: segments = a | b | c | d |         g
-    elif number == 4: segments =     b | c |         f | g
-    elif number == 5: segments = a |     c | d     | f | g
-    elif number == 6: segments = a |     c | d | e | f | g
-    elif number == 7: segments = a | b | c                
-    elif number == 8: segments = a | b | c | d | e | f | g
-    elif number == 9: segments = a | b | c | d     | f | g
-    elif number == 0: segments = a | b | c | d | e | f 
-    elif number == ' ': segments = 0
-    elif number == 'c': segments = g | e | d
-    elif number == '-': segments = g
-    else : segments = 0
+  for x in range(0,len(panel)):
+    shiftout(panel[x])
 
-    y=0
-    while(y<8):
-       GPIO.output(segmentClock,GPIO.LOW)
-       GPIO.output(segmentData,segments & 1 << (7-y))
-       GPIO.output(segmentClock,GPIO.HIGH)
-       y += 1
+for i in range (999999):
+   drawinteger(i)
+   time.sleep(1)
 
-while (number < 10):
-    showNumber(number)
-    number = number+1
-    print number
-    time.sleep(1)
-
-GPIO.cleanup()
-
-
+#counter = 0
+#while True:
+#   shiftout(numbers[counter])
+#   counter = counter + 1
+#   counter = counter%10
+#   time.sleep(1)
